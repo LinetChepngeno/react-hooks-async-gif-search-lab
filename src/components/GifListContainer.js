5888078f5be7a5a1ff12d9cadb5a1ff76cadb24d
Linet Chepngeno <linetmutai@gmail.com>
2024-02-17 17:50:47 +0300

    completed assignment

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import GifList from "./GifList";
+import GifSearch from "./GifSearch";
+
+function GifListContainer() {
+  const [gifs, setGifs] = useState([]);
+  const [query, setQuery] = useState("dolphins");
+
+  useEffect(() => {
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=mnSaK29LmPugKAOZ4vGjXt3SR6LSFU1B`
+    )
+      .then((r) => r.json())
+      .then(({ data }) => {
+        const gifs = data.map((gif) => ({ url: gif.images.original.url }));
+        setGifs(gifs);
+      });
+  }, [query]);
+
+  return (
+    <div style={{ display: "flex" }}>
+      <GifList gifs={gifs} />
+      <GifSearch onSubmitQuery={setQuery} />
+    </div>
+  );
+}
+
+export default GifListContainer;
